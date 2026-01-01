29b9cd26da6d740e3fec006737120d72443f2e0f
J G AKARSH <jgakarsh@gmail.com>
2026-01-01 23:00:43 +0530

    only print statement

--- src/string_Builder.java
+++ src/string_Builder.java
@@ -1,6 +1,7 @@
 public class string_Builder {
     static void main() {
         System.out.println("Hello");
-
+        StringBuilder sb=new StringBuilder("Akarsh");
+        System.out.println(sb);
     }
 }
